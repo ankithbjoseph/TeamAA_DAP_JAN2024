6b49be7f578e12a436dfb793f2e925b41ce8f685
Ankith Babu Joseph <x23185813@student.ncirl.ie>
2024-05-01 20:05:05 +0100

    finalizing locations

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -341,7 +341,7 @@ locations = [
     "North Wall Quay/Samuel Beckett bridge East",
     "North Wall Quay/Samuel Beckett bridge West",
     "O'Connell st/Princes st North",
-    "Phibsborough Rd/Enniskerry Road can use this for conclusion",
+    "Phibsborough Rd/Enniskerry Road",
     "Richmond st south/Portabello Harbour inbound",
     "Richmond st south/Portabello Harbour outbound",
 ]
@@ -393,25 +393,22 @@ def createpage_0():
                                 ### Enviornmental variables
                                 - **_id**: Unique identifier for each data entry.
                                 - **date**: Date and time of the recorded data.
-                                - **temperature_2m**: Temperature at 2 meters above ground level in Celsius.
-                                - **relative_humidity_2m**: Relative humidity at 2 meters above ground level, expressed as a percentage.
-                                - **dew_point_2m**: Dew point temperature at 2 meters above ground level in Celsius.
-                                - **apparent_temperature**: Perceived temperature, taking into account factors like humidity and wind, in Celsius.
-                                - **precipitation**: Total precipitation in millimeters.
-                                - **rain**: Amount of rainfall in millimeters.
-                                - **snowfall**: Amount of snowfall in millimeters.
-                                - **weather_code**: Numerical code representing the weather conditions.
-                                - **cloud_cover**: Percentage of sky covered by clouds.
-                                - **wind_speed_10m**: Wind speed at 10 meters above ground level in meters per second.
-                                - **wind_direction_10m**: Wind direction at 10 meters above ground level in degrees.
-                                - **is_day**: Binary indicator (0 or 1) indicating whether it's daytime.
-                                - **sunshine_duration**: Duration of sunshine in minutes.
-                                - **pm10**: Particulate Matter (PM10) concentration in micrograms per cubic meter.
-                                - **pm2_5**: Particulate Matter (PM2.5) concentration in micrograms per cubic meter.
-                                - **carbon_monoxide**: Carbon Monoxide (CO) concentration in micrograms per cubic meter.
-                                - **nitrogen_dioxide**: Nitrogen Dioxide (NO2) concentration in micrograms per cubic meter.
-                                - **sulphur_dioxide**: Sulphur Dioxide (SO2) concentration in micrograms per cubic meter.
-                                - **dust**: Dust concentration in micrograms per cubic meter.
+                                - **temperature_2m**(°C): Temperature at 2 meters above ground level in Celsius.
+                                - **relative_humidity_2m**(%): Relative humidity at 2 meters above ground level, expressed as a percentage.
+                                - **dew_point_2m**(°C): Dew point temperature at 2 meters above ground level in Celsius.
+                                - **apparent_temperature**(°C): Perceived temperature, taking into account factors like humidity and wind, in Celsius.
+                                - **precipitation**(mm): Total precipitation in millimeters.
+                                - **rain**(mm): Amount of rainfall in millimeters.
+                                - **snowfall**(cm): Amount of snowfall in centimeter.
+                                - **cloud_cover**(%): Percentage of sky covered by clouds.
+                                - **wind_speed_10m**(km/h): Wind speed at 10 meters above ground level in kilometers per hour.
+                                - **sunshine_duration**(Seconds): Duration of sunshine in seconds.
+                                - **pm10**(μg/m³): Particulate Matter (PM10) concentration in micrograms per cubic meter.
+                                - **pm2_5**(μg/m³): Particulate Matter (PM2.5) concentration in micrograms per cubic meter.
+                                - **carbon_monoxide**(μg/m³): Carbon Monoxide (CO) concentration in micrograms per cubic meter.
+                                - **nitrogen_dioxide**(μg/m³): Nitrogen Dioxide (NO2) concentration in micrograms per cubic meter.
+                                - **sulphur_dioxide**(μg/m³): Sulphur Dioxide (SO2) concentration in micrograms per cubic meter.
+                                - **dust**(μg/m³): Dust concentration in micrograms per cubic meter.
                                 - **european_aqi**: European Air Quality Index (AQI) calculated based on various pollutant concentrations.
                                 - **european_aqi_pm2_5**: European AQI specifically calculated for PM2.5.
                                 - **european_aqi_pm10**: European AQI specifically calculated for PM10.
@@ -422,12 +419,10 @@ def createpage_0():
             pn.pane.Markdown("""
                                  ### Counter Locations
                                 - Aston Quay/Fitzgeralds
-                                - Bachelors walk/Bachelors way
                                 - Baggot st lower/Wilton tce inbound
                                 - Baggot st upper/Mespil rd/Bank
                                 - Capel st/Mary street
                                 - College Green/Bank Of Ireland
-                                - College Green/Church Lane
                                 - College st/Westmoreland st
                                 - D'olier st/Burgh Quay
                                 - Dame Street/Londis
@@ -436,21 +431,13 @@ def createpage_0():
                                 - Grafton Street/CompuB
                                 - Grand Canal st upp/Clanwilliam place
                                 - Grand Canal st upp/Clanwilliam place/Google
-                                - Henry Street/Coles Lane/Dunnes
                                 - Mary st/Jervis st
-                                - Newcomen Bridge/Charleville mall inbound
-                                - Newcomen Bridge/Charleville mall outbound
                                 - North Wall Quay/Samuel Beckett bridge East
                                 - North Wall Quay/Samuel Beckett bridge West
-                                - O'Connell St/Parnell St/AIB
-                                - O'Connell St/Pennys Pedestrian
                                 - O'Connell st/Princes st North
                                 - Phibsborough Rd/Enniskerry Road
                                 - Richmond st south/Portabello Harbour inbound
                                 - Richmond st south/Portabello Harbour outbound
-                                - Talbot st/Guineys
-                                - Westmoreland Street East/Fleet street
-                                - Westmoreland Street West/Carrolls
                                  
                                  """),
         ),
