ae6aa378fc2eaca04bc0c2a180c645a549bb981b
alphonszacharia <alphonszacharia@gmail.com>
2024-05-02 19:01:43 +0100

    dash

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -13,6 +13,7 @@ from folium.plugins import MarkerCluster
 # Apply the CSS class to Panel components
 pn.extension()
 
+#connection with PostgreSQL
 postgres_host = os.getenv("POSTGRES_HOST", "localhost")
 warnings.filterwarnings("ignore", category=UserWarning)
 postgres_connect = f"postgresql://dap:dap@{postgres_host}:5432/projectdb"
@@ -38,7 +39,7 @@ def create_map():
 
     return m
 
-
+#function for displaying 20 datasets from final table
 def get_dataset():
     try:
         query_string = """
@@ -62,7 +63,7 @@ def get_dataset():
     except exc.SQLAlchemyError as db_error:
         print("Database error:", db_error)
 
-
+#function to display corelation b/w variables
 def create_scatter_plot(column, location, daterange):
     (start_date, end_date) = daterange
 
@@ -113,7 +114,7 @@ def create_scatter_plot(column, location, daterange):
     except exc.SQLAlchemyError as db_error:
         print("Database error:", db_error)
 
-
+#finction to display the relationship b/w two variables using line plot
 def create_line_plot(var, loc, daterange, avgby):
     match avgby:
         case "Daily":
@@ -234,8 +235,6 @@ locations = [
     "Richmond st south/Portabello Harbour outbound",
 ]
 
-
-#############################
 button_4 = pn.widgets.Button(
     name="Project Report",
     button_type="primary",
@@ -269,7 +268,7 @@ button_2.on_click(lambda event: show_page("page_2"))
 button_3.on_click(lambda event: show_page("page_3"))
 button_4.on_click(lambda event: show_page("page_4"))
 
-
+#function for each page in dashboard
 def createpage_0():
     # Create Panel app
     map_viewer = pn.pane.plot.Folium(create_map(), height=400)
@@ -452,8 +451,9 @@ mapping = {
     "page_3": createpage_3(),
     "page_4": createpage_4(),
 }
-
+#defining main area in dashboard
 main_area = pn.Column(mapping["page_0"], width=1190)
+#defining side bar
 sidebar = pn.Column(
     button_0,
     button_2,
